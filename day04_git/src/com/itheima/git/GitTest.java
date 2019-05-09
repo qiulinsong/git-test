f2f48f447bc2c3615909a0914e91d83372b59ac3
qiulinsong <2275998245@qq.com>
2019-05-09 17:52:57 +0800

    添加测试方法clone

--- day04_git/src/com/itheima/git/GitTest.java
+++ day04_git/src/com/itheima/git/GitTest.java
@@ -4,5 +4,10 @@ public class GitTest {
 
     public static void main(String[] args) {
         System.out.println("测试git");
+        cloneTest();
+    }
+
+    public static void cloneTest(){
+        System.out.println("测试clone");
     }
 }
